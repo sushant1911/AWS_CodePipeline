40050fd2dfe9238bd62c34d542e92db4c2a1201d
sushantsuman <sushantcvrcebtech17@gmail.com>
2024-08-27 23:57:44 +0530

    aws commit

---
+++ src/main/java/com/examle/deployeAws/deployeAws/TestController.java
@@ -0,0 +1,14 @@
+package com.examle.deployeAws.deployeAws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/welcome")
+    public String welcome()
+    {
+        return "welcome to aws deployment";
+    }
+}
