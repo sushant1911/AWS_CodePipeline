7dd64d25474ab6e6abb1cc66910a155e3fe3e8b7
sushantsuman <sushantcvrcebtech17@gmail.com>
2024-08-27 22:46:57 +0530

    second commit

---
+++ src/main/java/com/examle/deployeAws/deployeAws/DeployeAwsApplication.java
@@ -0,0 +1,13 @@
+package com.examle.deployeAws.deployeAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployeAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployeAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examle/deployeAws/deployeAws/DeployeAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examle.deployeAws.deployeAws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployeAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
